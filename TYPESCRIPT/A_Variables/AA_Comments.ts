914f3d9ef53baf784346855b7bdb619a3702315d
veeresshh <161887965+veeresshh@users.noreply.github.com>
2026-03-07 00:47:55 +0530

    Add files via upload

---
+++ TYPESCRIPT/A_Variables/AA_Comments.ts
@@ -0,0 +1,32 @@
+//SHORTCUTS
+
+//Single line comment    
+
+    // Windows :    ctrl + /
+    // Mac:         Cmd + /
+
+
+// Multiline comment   
+
+    // Windows:     Shift + Alt + A
+    // Mac:         Shift + Option + A
+
+
+
+//Examples:
+
+// Single line comment 
+
+// console.log("welcome")
+
+
+// Multiline comment   
+
+/*
+
+console.log("welcome")
+console.log("welcome")
+console.log("welcome")
+console.log("welcome")
+
+*/
